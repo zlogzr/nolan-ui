bf2ed947ebd03b58c4b087f6e508f721102cca52
zhangyang <2224143549@qq.com>
2022-11-30 18:07:00 +0800

    fix: fatherrc配置

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,4 +1,6 @@
 export default {
   // more father 4 config: https://github.com/umijs/father-next/blob/master/docs/config.md
-  sideEffects: ['./src/**/style/**'],
+  cssModules: {
+    generateScopedName: '[name][local]_[hash:base64:5]',
+  },
 };
